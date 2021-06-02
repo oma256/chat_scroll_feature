130a79c805165e24b81ea1db36848885a95e925d
Omurbek Dulatov <oma.dulatov@gmail.com>
2021-06-02 10:36:30 +0600

    initial files and added js code for fine item

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+const input = document.getElementById('input_id');
 
-console.log('Hello World!');
+
+input.onkeyup = (e) => {
+  const val = e.target.value;
+  const chat_item = document.getElementById(val);
+  const contentBlock = document.getElementById('content_id');
+  
+  chat_item.scrollIntoView({
+    behavior: "smooth"
+  });
+
+  contentBlock.childNodes.forEach(item => {
+    item.classList?.remove('selected');
+  })
+
+  chat_item.classList.add('selected');
+}
